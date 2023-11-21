11abd1680d432206faad51969b6cf91996252ba6
iSayU <103714465+nguyenvinhhuy123@users.noreply.github.com>
2023-11-21 15:58:35 +0700

    refactor: change code format

--- backend/src/repositories/model.js
+++ backend/src/repositories/model.js
@@ -1,92 +1,94 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        minlength: 6,
-        maxlength: 20,
-        unique: true,
+      type: String,
+      required: true,
+      minlength: 6,
+      maxlength: 20,
+      unique: true,
     },
 
     password: {
-        type: String,
-        required: true,
-        minlength: 6,
+      type: String,
+      required: true,
+      minlength: 6,
     },
 
     balance: {
-        type: Number,
-        default: 10,
+      type: Number,
+      default: 10,
     },
-}, { timestamps: true } // show when it is created and updated 
-);
+  },
+  { timestamps: true } // show when it is created and updated
+)
 
 const printSchema = new mongoose.Schema({
-    brandName: {
-        type: String,
-        required: true
-    },
-    printerModel: {
-        type: String,
-        required: true
-    },
-    location: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-});
+  brandName: {
+    type: String,
+    required: true,
+  },
+  printerModel: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+})
 
 const fileRecord = new mongoose.Schema({
-    studentID: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-        },
-    ],
-    fileName: {
-        type: String,
-        required: true
+  studentID: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
     },
-});
+  ],
+  fileName: {
+    type: String,
+    required: true,
+  },
+})
 const printingHistory = new mongoose.Schema({
-    studentID: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-        },
-    ],
-    printerID: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Printer",
-        },
-    ],
-    fileID: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "FileRecord",
-        },
-    ],
-    startTime: {
-        type: Date,
-        required: true
+  studentID: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
     },
-    endTime: {
-        type: Date,
-        required: true
+  ],
+  printerID: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Printer',
     },
-    numberOfPage: {
-        type: Number,
-        required: true
-    }
-});
-let Printer = mongoose.model("Printer", printSchema);
-let User = mongoose.model("User", userSchema);
-let FileRecord = mongoose.model("FileRecord", fileRecord);
-let FileHistory = mongoose.model("FileHistory", fileRecord);
-module.exports = { Printer, User, FileHistory, FileRecord };
+  ],
+  fileID: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'FileRecord',
+    },
+  ],
+  startTime: {
+    type: Date,
+    required: true,
+  },
+  endTime: {
+    type: Date,
+    required: true,
+  },
+  numberOfPage: {
+    type: Number,
+    required: true,
+  },
+})
+let Printer = mongoose.model('Printer', printSchema)
+let User = mongoose.model('User', userSchema)
+let FileRecord = mongoose.model('FileRecord', fileRecord)
+let FileHistory = mongoose.model('FileHistory', printingHistory)
+module.exports = { Printer, User, FileHistory, FileRecord }
