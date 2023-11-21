54b6fc086e081ba132507688c19d6efaa1a74cac
KienLe123456 <kien.le123@hcmut.edu.vn>
2023-11-21 15:11:50 +0700

    somethingmore

--- backend/src/repositories/model.js
+++ backend/src/repositories/model.js
@@ -9,38 +9,84 @@ const userSchema = new mongoose.Schema({
         unique: true,
     },
 
-    password :{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: true,
         minlength: 6,
     },
 
-    adminSPSO: {
-        type: Boolean,
-        default : false,
+    balance: {
+        type: Number,
+        default: 10,
     },
-}, {timestamps:true} // show when it is created and updated 
+}, { timestamps: true } // show when it is created and updated 
 );
 
 const printSchema = new mongoose.Schema({
     brandName: {
-        type: String
-        //,required: true
+        type: String,
+        required: true
     },
-    Printer_Model: {
-        type: String
-        //,required: true
+    printerModel: {
+        type: String,
+        required: true
     },
     location: {
         type: String,
         required: true
     },
     description: {
-        type: String
-        // ,required: true
+        type: String,
+        required: true
     },
 });
 
+const fileRecord = new mongoose.Schema({
+    studentID: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+    fileName: {
+        type: String,
+        required: true
+    },
+});
+const printingHistory = new mongoose.Schema({
+    studentID: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+    ],
+    printerID: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Printer",
+        },
+    ],
+    fileName: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "FileRecord",
+        },
+    ],
+    printingStartTime: {
+        type: String,
+        required: true
+    },
+    printingEndTime: {
+        type: String,
+        required: true
+    },
+    numberOfPage: {
+        type: Number,
+        required: true
+    }
+});
 let Printer = mongoose.model("Printer", printSchema);
 let User = mongoose.model("User", userSchema);
-module.exports = { Printer, User };
+let FileRecord = mongoose.model("FileRecord", fileRecord);
+let FileHistory = mongoose.model("FileHistory", fileRecord);
+module.exports = { Printer, User, FileHistory, FileRecord };
