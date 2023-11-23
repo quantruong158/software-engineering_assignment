a2a6bd397ca176b2a190108a19dcb2e0082ec882
KienLe123456 <kien.le123@hcmut.edu.vn>
2023-11-23 21:24:20 +0700

    Change findbyID

--- backend/src/repositories/studentRepo.js
+++ backend/src/repositories/studentRepo.js
@@ -1,13 +1,12 @@
 const { User } = require("../repositories/model");
 const studentRepo = {
     getBalanceByID: async (studentID) => {
-        const user = await User.findOne({ studentID }); // find by username - attribute in the model.js
-        // const user = await User.findByID(studentID) ; // find by ID - the auto ID created by mongoose
+        const user = await User.findByID(studentID) ; 
         return user.balance;
     },
     decrementBalanceByID: async (studentID, amount) => {
-        const user = await User.findOne({ studentID });
-        // const user = await User.findByID(studentID) ;
+        //const user = await User.findOne({ studentID });
+        const user = await User.findByID(studentID) ;
         user.balance -= amount;
         await user.save();
         return;
